3b4eacb5e1eef02b380d60259e1d993f0ca2144f
Viktor Dmytrenko <viktor.dmytrenko-19@gmail.com>
2020-05-19 21:12:56 +0300

    change some validations

--- middlewares/user.validation.middleware.js
+++ middlewares/user.validation.middleware.js
@@ -1,3 +1,4 @@
+
 const { user } = require('../models/user');
 const createUserValid = (req, res, next) => {
     // TODO: Implement validatior for user entity during creation
@@ -10,7 +11,12 @@ const createUserValid = (req, res, next) => {
                 }
             }
     //  next validation are only for demonstration
-        if (!newUser.email || !newUser.email.includes('@')) throw('email problems');
+        const tel = newUser.phoneNumber
+            .trim()
+            .replace(/-|\s/g, '');
+        req.body.phoneNumber = tel;
+        if (tel.search(/\+380\d{9}$/) === -1) throw('use correct format for phone number');
+        if (!newUser.email || !newUser.email.includes('@')) throw('email is not valid');
         if (newUser.password === undefined) throw('No password')
         if (newUser.password === '' || newUser.password.length < 3) {
             throw ('Error: Too short password');
@@ -18,7 +24,9 @@ const createUserValid = (req, res, next) => {
         console.log('newUser from createUserValid', newUser);
         next()
     } catch (err) {
+
         console.log(err);
+        res.status(400).send(JSON.stringify(err));
         res.err = err;
     } 
    //    not ready // stub only!!!!!
