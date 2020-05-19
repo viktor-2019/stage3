66798e40952ebd87cc11d43d795fb9be07aa6b3d
Viktor Dmytrenko <viktor.dmytrenko-19@gmail.com>
2020-05-19 21:15:53 +0300

    first strings in routers

--- routes/fighterRoutes.js
+++ routes/fighterRoutes.js
@@ -6,5 +6,34 @@ const { createFighterValid, updateFighterValid } = require('../middlewares/fight
 const router = Router();
 
 // TODO: Implement route controllers for fighter
+router.get('/', (req, res, next) => {
+  console.log('this is fighter router.get req', req.body);
+  // let us stop for begining
+  res.end();
+});
+router.get('/id', (req, res, next) => {
+  console.log('this is a fighter router.get/id ', req.params.id);
+  res.end();
+});
+
+
+router.post('/', (req, res, next) => {
+  console.log('this is a fighter router.post: ', req.params.id);
+  res.end();
+});
+
+router.put('/id', (req, res, next) => {
+  console.log('this is a fighter router.PUT/id ', req.params.id);
+  res.end();
+});
+
+router.delete('/id', (req, res, next) => {
+  console.log('this is a fighter router.Delete/id ', req.params.id);
+  res.end();
+});
+
+
+
+
 
 module.exports = router;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,15 +9,25 @@ router.post('/', createUserValid, (req, res, next) => {
   try {
       const { saveUser } = UserService;
       const result = saveUser(req.body);
+      console.log('result from userRouter: ', result);
       if (result) {
         next();
-      }
-      
+      }    
   } catch (err) {
       res.err = err;
   }   
 }, responseMiddleware);
 
+router.post('/id', (req, res, next) => {
+  console.log('this is a user router.post/id ', req.params.id);
+  res.end();
+})
+
+router.get('/', (req, res, next) => {
+  console.log('this is user router.get req', req.header);
+  // let us stop for begining
+  res.end();
+})
 
 
 // not ready
